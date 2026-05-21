19db9ee980fb7a0dd42692ba0e36f8bfc25e60b8
Jack Mayo <jackomayo@gmail.com/>
2026-05-21 09:24:43 +0100

    test(s2): snapshot compatible-tag lists for 5-platform matrix
    Five platform configurations (linux-x86_64-gnu at 3.12, linux-aarch64-gnu
    at 3.11, linux-x86_64-musl at 3.12, macos-x86_64 at 3.12, macos-arm64
    at 3.12) are pinned via insta snapshots so reordering bugs become
    visible diffs in review.

--- src/wheel/compat.rs
+++ src/wheel/compat.rs
@@ -385,4 +385,100 @@ mod tests {
         assert!(compat.rank_of(&m_12).unwrap() < compat.rank_of(&m_11).unwrap());
         assert_eq!(compat.rank_of(&m_13), None);
     }
+
+    fn format_compat_for_snapshot(compat: &CompatibleTags) -> String {
+        compat.ordered().iter()
+            .enumerate()
+            .map(|(i, t)| format!("{:3}: {}", i, render_tag(t)))
+            .collect::<Vec<_>>()
+            .join("\n")
+    }
+
+    fn render_tag(t: &Tag) -> String {
+        format!("{}-{}-{}", render_python(&t.python), render_abi(&t.abi), render_platform(&t.plat))
+    }
+
+    fn render_python(p: &PythonTag) -> String {
+        match p {
+            PythonTag::CPython(maj, min) => format!("cp{}{}", maj, min),
+            PythonTag::Py(maj, Some(min)) => format!("py{}{}", maj, min),
+            PythonTag::Py(maj, None) => format!("py{}", maj),
+            PythonTag::Other(s) => s.clone(),
+        }
+    }
+
+    fn render_abi(a: &AbiTag) -> String {
+        match a {
+            AbiTag::CPython(maj, min) => format!("cp{}{}", maj, min),
+            AbiTag::Abi3 => "abi3".into(),
+            AbiTag::None => "none".into(),
+            AbiTag::Other(s) => s.clone(),
+        }
+    }
+
+    fn render_platform(p: &PlatformTag) -> String {
+        let arch_l = |a: &LinuxArch| match a { LinuxArch::X86_64 => "x86_64", LinuxArch::Aarch64 => "aarch64" };
+        let arch_m = |a: &MacArch| match a {
+            MacArch::X86_64 => "x86_64", MacArch::Arm64 => "arm64", MacArch::Universal2 => "universal2",
+        };
+        match p {
+            PlatformTag::Any => "any".into(),
+            PlatformTag::ManyLinux { major, minor, arch } =>
+                format!("manylinux_{}_{}_{}", major, minor, arch_l(arch)),
+            PlatformTag::MuslLinux { major, minor, arch } =>
+                format!("musllinux_{}_{}_{}", major, minor, arch_l(arch)),
+            PlatformTag::MacOs { major, minor, arch } =>
+                format!("macosx_{}_{}_{}", major, minor, arch_m(arch)),
+            PlatformTag::Other(s) => s.clone(),
+        }
+    }
+
+    fn platform_for_snapshot(target: &str, mnl: Option<&str>, msl: Option<&str>, mac: Option<&str>) -> crate::config::Platform {
+        crate::config::Platform {
+            target: target.into(),
+            manylinux: mnl.map(String::from),
+            musllinux: msl.map(String::from),
+            macos_min: mac.map(String::from),
+        }
+    }
+
+    #[test]
+    fn compat_snapshot_linux_x86_64_gnu_3_12() {
+        use crate::config::PythonVersion;
+        let p = platform_for_snapshot("x86_64-unknown-linux-gnu", Some("2_28"), None, None);
+        let c = build_compatible_tags(&p, PythonVersion(3, 12));
+        insta::assert_snapshot!(format_compat_for_snapshot(&c));
+    }
+
+    #[test]
+    fn compat_snapshot_linux_aarch64_gnu_3_11() {
+        use crate::config::PythonVersion;
+        let p = platform_for_snapshot("aarch64-unknown-linux-gnu", Some("2_17"), None, None);
+        let c = build_compatible_tags(&p, PythonVersion(3, 11));
+        insta::assert_snapshot!(format_compat_for_snapshot(&c));
+    }
+
+    #[test]
+    fn compat_snapshot_linux_x86_64_musl_3_12() {
+        use crate::config::PythonVersion;
+        let p = platform_for_snapshot("x86_64-unknown-linux-musl", None, Some("1_2"), None);
+        let c = build_compatible_tags(&p, PythonVersion(3, 12));
+        insta::assert_snapshot!(format_compat_for_snapshot(&c));
+    }
+
+    #[test]
+    fn compat_snapshot_macos_x86_64_3_12() {
+        use crate::config::PythonVersion;
+        let p = platform_for_snapshot("x86_64-apple-darwin", None, None, Some("11.0"));
+        let c = build_compatible_tags(&p, PythonVersion(3, 12));
+        insta::assert_snapshot!(format_compat_for_snapshot(&c));
+    }
+
+    #[test]
+    fn compat_snapshot_macos_arm64_3_12() {
+        use crate::config::PythonVersion;
+        let p = platform_for_snapshot("aarch64-apple-darwin", None, None, Some("11.0"));
+        let c = build_compatible_tags(&p, PythonVersion(3, 12));
+        insta::assert_snapshot!(format_compat_for_snapshot(&c));
+    }
 }
